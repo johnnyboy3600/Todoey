54afd393a8e50171930c24814b4aaf0bf2c439c4
John Murphy <johnnyboy3600@sky.com>
2019-04-09 11:08:19 +0100

    Convert to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by John Murphy on 09/04/2019.
+//  Copyright © 2019 John Murphy. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
